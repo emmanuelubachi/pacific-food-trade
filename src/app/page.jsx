e708917049eba2228f9d8bc71de84c5d4921c7ba
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-17 05:44:22 +0400

    add jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FeedingThePacific = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Feeding the pacific</h1>
+      </main>
+    </>
+  );
+};
+
+export default FeedingThePacific;
