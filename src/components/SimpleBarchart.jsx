13b07b33435b47b2e0de6ca98ac9f27210cf26ff
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-18 22:56:24 +0400

    add component

---
+++ src/components/SimpleBarchart.jsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import * as echarts from "echarts";
+import VintageTheme from "/public/vintage.project.json";
+import ReactECharts from "echarts-for-react"; // or var ReactECharts = require('echarts-for-react');
+
+export default function SimpleBarchart() {
+  echarts.registerTheme("vintage", VintageTheme.theme);
+  echarts.registerTheme("mytheme", {
+    color: "#919e8b",
+  });
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: "bar",
+      },
+    ],
+  };
+  return (
+    <>
+      <ReactECharts
+        option={option}
+        style={{ height: 400 }}
+        theme={"vintage"}
+        opts={{ renderer: "svg" }}
+      />
+    </>
+  );
+}
