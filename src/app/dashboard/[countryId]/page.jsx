ef596b8efec16f78954c39fb454ab84980cdd048
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-22 02:14:12 +0400

    address error

--- src/app/dashboard/[countryId]/page.jsx
+++ src/app/dashboard/[countryId]/page.jsx
@@ -7,6 +7,15 @@ const CountryPage = ({ params }) => {
 
   const data = TopFoodData.filter((foodData) => foodData.iso3 === countryISO);
 
+  if (data.length === 0) {
+    // Handle the case where no matching data is found, e.g., show an error message.
+    return (
+      <main>
+        <p>No data found for the specified country.</p>
+      </main>
+    );
+  }
+
   return (
     <main>
       <TopFoods data={data[0]} />
