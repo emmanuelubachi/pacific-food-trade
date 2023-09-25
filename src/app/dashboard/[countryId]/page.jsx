226831c68d06daa4edb08ca6eb2f075799447a23
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-25 13:55:11 +0400

    add page

--- src/app/dashboard/[countryId]/page.jsx
+++ src/app/dashboard/[countryId]/page.jsx
@@ -1,26 +1,69 @@
 import React from "react";
-import TopFoods from "@/components/sections/TopFoods";
-import TopFoodData from "@/data/food_trade_by_year.json";
+// import { useRouter } from "next/navigation";
+
+import FoodImport from "@/components/sections/FoodImport";
+import FoodExport from "@/components/sections/FoodExport";
+import FoodTrend from "@/components/sections/FoodTrend";
+import Hero from "@/components/sections/Hero";
+
+import Country from "@/data/pacific_country.json";
+import TopFoodExportData from "@/data/food_exports_by_year.json";
+import TopFoodImportData from "@/data/food_imports_by_year.json";
+import FoodTradeTrend from "../../../data/food_trade_trend.json";
+import Navigation from "@/components/sections/navigation";
 
 const CountryPage = ({ params }) => {
   const countryISO = params.countryId;
 
-  const data = TopFoodData.filter((foodData) => foodData.iso3 === countryISO);
+  const country = Country.filter(
+    (countryinfo) => countryinfo.iso3 === countryISO
+  );
+  const exportData = TopFoodExportData.filter(
+    (foodData) => foodData.iso3 === countryISO
+  );
+  const importData = TopFoodImportData.filter(
+    (foodData) => foodData.iso3 === countryISO
+  );
+  const tradeTrendData = FoodTradeTrend.filter(
+    (trendData) => trendData.iso3 === countryISO
+  );
 
-  if (data.length === 0) {
+  if (country.length === 0) {
     // Handle the case where no matching data is found, e.g., show an error message.
     return (
-      <main>
+      <main className="m-auto flex justify-center items-center py-24 ">
         <p>No data found for the specified country.</p>
       </main>
     );
-  }
+  } else {
+    return (
+      <main>
+        {country.length && <Hero countryName={country[0].country} />}
+        <section className="bg-gradient-to-b from-pri-12 to-white">
+          <div
+            className=" h-96 max-w-screen-xl px-4 mx-auto 
+            md:gap-8 
+            xl:gap-0 
+            py-8 
+          bg-white 
+          rounded-t-3xl 
+          shadow-pri-12 
+          shadow-[0_30px_70px_-50px_rgba(0,0,0,0.3)]"
+          >
+            {importData.length && (
+              <FoodImport data={importData} countryName={country[0].country} />
+            )}
+            {exportData.length && (
+              <FoodExport data={exportData} countryName={country[0].country} />
+            )}
 
-  return (
-    <main>
-      <TopFoods data={data} />
-    </main>
-  );
+            {tradeTrendData && <FoodTrend data={tradeTrendData} />}
+          </div>
+        </section>
+        {/* <Navigation /> */}
+      </main>
+    );
+  }
 };
 
 export default CountryPage;
