f0952abcec511444dcadc27380703cbffa7f922a
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-25 14:03:49 +0400

    add chart Frame

---
+++ src/components/chartFrame/YearFilter.jsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Treemap from "@/components/charts/Treemap";
+import { ComboboxDemo } from "@/components/ui/Combobox";
+
+const YearFilter = ({ data, options }) => {
+  const Year = options;
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(Year[0].value);
+  const [selectedData, setSelectedData] = useState(null);
+
+  useEffect(() => {
+    // Use the data passed as props to filter and select the object with the chosen year.
+    const selectedYear = parseInt(value);
+    const selectedObject = data.find((item) => item.Year === selectedYear);
+
+    setSelectedData(selectedObject);
+  }, [value, data]);
+
+  const handleSelectValue = (currentValue) => {
+    setValue(currentValue);
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="m-auto max-w-screen-xl pt-4">
+        <div
+          className="gap-4 items-center
+              justify-center
+              mx-auto 
+              md:grid md:grid-cols-1
+              xl:gap-16"
+        >
+          <div className=" sm:h-full min-h-screen ">
+            <div className="flex gap-2 justify-end">
+              <ComboboxDemo
+                options={Year}
+                defaultValue={Year[0].value}
+                defaultLabel={Year[0].label}
+                handleSelectValue={handleSelectValue}
+                setOpen={setOpen}
+                value={value}
+                open={open}
+              />
+            </div>
+            {/* Display the selectedData in Treemap */}
+            {selectedData && <Treemap data={selectedData.Data} />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default YearFilter;
