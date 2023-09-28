a77b5090e01615037733b362d649c32b5284ad4d
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-28 19:53:50 +0400

    add Filter frame

---
+++ src/components/chartFrame/MapFilters.jsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Treemap from "@/components/charts/Treemap";
+import { ComboboxDemo } from "@/components/ui/Combobox";
+import Loading from "../body/Loading";
+import ChoroplethMap from "@/components/charts/ChoroplethMap";
+
+const MapFilters = ({ data, options }) => {
+  const Year = options;
+
+  const [countries, setCountries] = useState([]);
+
+  // const load = () => {
+  //   const loadCountriesTask = new LoadCountriesTask();
+  //   loadCountriesTask.load(setCountries);
+  // };
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(Year[0].value);
+  const [selectedData, setSelectedData] = useState(null);
+
+  useEffect(() => {
+    // Use the data passed as props to filter and select the object with the chosen year.
+    const selectedYear = parseInt(value);
+    const selectedObject = data.find((item) => item.Year === selectedYear);
+
+    setSelectedData(selectedObject);
+  }, [value, data]);
+
+  const handleSelectValue = (currentValue) => {
+    setValue(currentValue);
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="m-auto max-w-screen-xl pt-4">
+        <div
+          className="gap-4 items-center
+              justify-center
+              mx-auto 
+              md:grid md:grid-cols-1
+              xl:gap-16"
+        >
+          <div className=" sm:h-full min-h-screen ">
+            <div className="flex gap-2 justify-end">
+              <ComboboxDemo
+                options={Year}
+                defaultValue={Year[0].value}
+                defaultLabel={Year[0].label}
+                handleSelectValue={handleSelectValue}
+                setOpen={setOpen}
+                value={value}
+                open={open}
+              />
+            </div>
+            {/* Display the selectedData in Map */}
+            {countries.length === 0 ? (
+              <Loading />
+            ) : (
+              <div />
+              // <ChoroplethMap countries={countries} />
+            )}
+            {/* {selectedData && <Treemap data={selectedData.Data} />} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MapFilters;
