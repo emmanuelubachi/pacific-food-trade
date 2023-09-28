7b6c2808546c260e9b1918e884cf7a218a03885e
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-28 19:54:50 +0400

    update task

--- src/tasks/LoadCountriesTask.js
+++ src/tasks/LoadCountriesTask.js
@@ -1,8 +1,18 @@
-import { features } from "@/data/countries.json";
+import countries from "@/data/countries.json";
 
 class LoadCountriesTask {
+  setState = null;
+  mapCountries = countries.features;
   load = (setState) => {
-    setState(features);
+    this.setState = setState;
+    //setState(countries.features);
+    // (data) => {this.#processData(data)},
+  };
+
+  #processData = (countriesData) => {
+    for (let i = 0; i < this.mapCountries.length; i++) {
+      const mapCountries = this.mapCountries[i];
+    }
   };
 }
 
