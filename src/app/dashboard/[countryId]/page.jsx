13350c07312d173854cfb25eaeaa5ea16a925d10
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-28 12:32:16 +0400

    update page

--- src/app/dashboard/[countryId]/page.jsx
+++ src/app/dashboard/[countryId]/page.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 // import { useRouter } from "next/navigation";
 
+import Country from "@/data/pacific_country.json";
+import TopFoodExportData from "@/data/food_exports_by_year.json";
+import TopFoodImportData from "@/data/food_imports_by_year.json";
+import FoodTradeTrend from "../../../data/food_trade_trend.json";
+
 import FoodImport from "@/components/sections/FoodImport";
 import FoodExport from "@/components/sections/FoodExport";
 import FoodTrend from "@/components/sections/FoodTrend";
 import Hero from "@/components/sections/Hero";
 
-import Country from "@/data/pacific_country.json";
-import TopFoodExportData from "@/data/food_exports_by_year.json";
-import TopFoodImportData from "@/data/food_imports_by_year.json";
-import FoodTradeTrend from "../../../data/food_trade_trend.json";
 import Navigation from "@/components/sections/navigation";
+import TradeMap from "@/components/sections/TradeMap";
 
 const CountryPage = ({ params }) => {
   const countryISO = params.countryId;
@@ -39,27 +41,18 @@ const CountryPage = ({ params }) => {
     return (
       <main>
         {country.length && <Hero countryName={country[0].country} />}
-        <section className="bg-gradient-to-b from-pri-12 to-white">
-          <div
-            className=" h-96 max-w-screen-xl px-4 mx-auto 
-            md:gap-8 
-            xl:gap-0 
-            py-8 
-          bg-white 
-          rounded-t-3xl 
-          shadow-pri-12 
-          shadow-[0_30px_70px_-50px_rgba(0,0,0,0.3)]"
-          >
-            {importData.length && (
-              <FoodImport data={importData} countryName={country[0].country} />
-            )}
-            {exportData.length && (
-              <FoodExport data={exportData} countryName={country[0].country} />
-            )}
 
-            {tradeTrendData && <FoodTrend data={tradeTrendData} />}
-          </div>
-        </section>
+        <TradeMap />
+
+        {/* {importData.length && (
+          <FoodImport data={importData} countryName={country[0].country} />
+        )} */}
+
+        {/* {exportData.length && (
+          <FoodExport data={exportData} countryName={country[0].country} />
+        )} */}
+
+        {tradeTrendData && <FoodTrend data={tradeTrendData} />}
         {/* <Navigation /> */}
       </main>
     );
