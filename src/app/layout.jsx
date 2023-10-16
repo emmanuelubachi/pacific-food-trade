dc9d517665917d0b0edb087fc24668b13ddc8960
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-17 00:01:51 +0400

    update layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
-import { Providers } from "./providers";
+import { Providers } from "../providers/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
